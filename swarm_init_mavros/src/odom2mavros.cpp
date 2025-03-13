e64a998cc41919f04567ad09a94d12912861f26a
XinChen-stars <1627938222@qq.com>
2025-03-13 17:06:25 +0800

    fix some little problems

--- swarm_init_mavros/src/odom2mavros.cpp
+++ swarm_init_mavros/src/odom2mavros.cpp
@@ -4,6 +4,7 @@
 
 nav_msgs::Odometry Odom;
 ros::Publisher swarm_pos_pub;
+bool is_swarm = true;
 bool odom_received = false;
 bool odom_init = false;
 bool swarm_init_offset_received = false;
@@ -22,26 +23,39 @@ double z_init_offset = 0;
 
 void Odom_cb(const nav_msgs::Odometry::ConstPtr &msg)
 {
-    odom_received = true;
-    ROS_INFO("Odom received");
-    Odom = *msg;
-    if (odom_count < 100)
+    static bool first_log = true;
+
+    if (first_log)
     {
-        x_odom_init_sum += Odom.pose.pose.position.x;
-        y_odom_init_sum += Odom.pose.pose.position.y;
-        z_odom_init_sum += Odom.pose.pose.position.z;
-        odom_count++;
+        ROS_INFO("Odom received");
+        first_log = false;
     }
-    else
+
+    odom_received = true;
+    Odom = *msg;
+
+    if (!odom_init)
     {
-        x_odom_init = x_odom_init_sum / 100;
-        y_odom_init = y_odom_init_sum / 100;
-        z_odom_init = z_odom_init_sum / 100;
-        odom_init = true;
-        ROS_INFO("Odom init !!!!!!!!!");
-        ROS_INFO("x_odom_init: %f", x_odom_init);
-        ROS_INFO("y_odom_init: %f", y_odom_init);
-        ROS_INFO("z_odom_init: %f", z_odom_init);
+        if (odom_count < 100)
+        {
+            x_odom_init_sum += Odom.pose.pose.position.x;
+            y_odom_init_sum += Odom.pose.pose.position.y;
+            z_odom_init_sum += Odom.pose.pose.position.z;
+            odom_count++;
+        }
+
+        if (odom_count == 100)  // 仅在收集满 100 次数据时计算
+        {
+            x_odom_init = x_odom_init_sum / 100;
+            y_odom_init = y_odom_init_sum / 100;
+            z_odom_init = z_odom_init_sum / 100;
+            odom_init = true;
+
+            ROS_INFO("Odom init completed:");
+            ROS_INFO("x_odom_init: %f", x_odom_init);
+            ROS_INFO("y_odom_init: %f", y_odom_init);
+            ROS_INFO("z_odom_init: %f", z_odom_init);
+        }
     }
 }
 
@@ -60,8 +74,10 @@ void SwarmInitOffset_cb(const geometry_msgs::PoseStamped::ConstPtr &msg)
 
 void PubSwarmPosetoMavros()
 {
+    if (!odom_received || !swarm_init_offset_received) return;
+    
     geometry_msgs::PoseStamped pose;
-    pose.header.stamp = ros::Time::now();
+    pose.header.stamp = Odom.header.stamp;
     pose.header.frame_id = "base_link";
     pose.pose.position.x = Odom.pose.pose.position.x + x_init_offset - x_odom_init;
     pose.pose.position.y = Odom.pose.pose.position.y + y_init_offset - y_odom_init;
@@ -76,8 +92,21 @@ void PubSwarmPosetoMavros()
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "Odom_to_mavros");
-  ros::NodeHandle nh;
+  ros::NodeHandle nh("~");
+
+  nh.param("is_swarm", is_swarm, bool (true));
+
+  ROS_INFO("is_swarm: %d", is_swarm);
   
+  if(!is_swarm)
+  {
+    swarm_init_offset_received = true;
+    swarm_init_offset.pose.position.x = 0;
+    swarm_init_offset.pose.position.y = 0;
+    swarm_init_offset.pose.position.z = 0;  
+  }
+
+
   ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry> ("/Odometry", 100, Odom_cb);
   ros::Subscriber swarm_init_offset_sub = nh.subscribe<geometry_msgs::PoseStamped> ("/initpose", 100, SwarmInitOffset_cb);
   swarm_pos_pub=nh.advertise<geometry_msgs::PoseStamped> ("/mavros/vision_pose/pose",10);
@@ -86,10 +115,7 @@ int main(int argc, char** argv)
   ROS_INFO("Odom to mavros node started");
   while (ros::ok())
   {
-    if (odom_init && swarm_init_offset_received)
-    {
-        PubSwarmPosetoMavros();
-    }
+    PubSwarmPosetoMavros(); 
     ros::spinOnce();
     rate.sleep();
   }
